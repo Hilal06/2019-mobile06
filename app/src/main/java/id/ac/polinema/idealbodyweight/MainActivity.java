2951d9df8a0ca4e22c55938fd3711c8611331607
Hilal06 <rifaulhilal06@gmail.com>
2019-10-02 21:49:10 +0700

    Tugas membuat interaksi antara BMIfragment dan resultFragment, serta mengcode ulang algoritma pada button try again pada resultFragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -2,28 +2,31 @@ package id.ac.polinema.idealbodyweight;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import id.ac.polinema.idealbodyweight.fragment.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragment.BMIFragment;
 import id.ac.polinema.idealbodyweight.fragment.BrocaIndexFragment;
 import id.ac.polinema.idealbodyweight.fragment.MenuFragment;
 import id.ac.polinema.idealbodyweight.fragment.ResultFragment;
-import id.ac.polinema.idealbodyweight.util.BrocaIndex;
 
 public class MainActivity extends AppCompatActivity implements
 		MenuFragment.OnFragmentInteractionListener,
 		BrocaIndexFragment.OnFragmentInteractionListener,
-		ResultFragment.OnFragmentInteractionListener {
+		ResultFragment.OnFragmentInteractionListener,
+		BMIFragment.OnFragmentInteractionListener {
 
 	// Deklarasikan atribut Fragment di sini
-	MenuFragment menuFragment;
+	private MenuFragment menuFragment;
 
 	private AboutFragment aboutFragment;
 	private BrocaIndexFragment brocaIndexFragment;
 	private ResultFragment resultFragment;
+	private BMIFragment bmiFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements
 		menuFragment = new MenuFragment();
 		brocaIndexFragment = new BrocaIndexFragment();
 		resultFragment = new ResultFragment();
+		bmiFragment = new BMIFragment();
 
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, menuFragment)
@@ -61,28 +65,48 @@ public class MainActivity extends AppCompatActivity implements
 	public void onBrocaIndexButtonClicked() {
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, brocaIndexFragment)
+				.addToBackStack(null)
 				.commit();
 
 	}
 
 	@Override
 	public void onBodyMassIndexButtonClicked() {
-
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, bmiFragment)
+				.addToBackStack(null)
+				.commit();
 	}
 
 
 	@Override
 	public void onCalculateBrocaIndexClicked(float index) {
-		resultFragment.setInformation(String.format("Your ideal weight is %.2f kg", index));
+		resultFragment.setInformation(String.format("Your ideal weight is %.2f kg ", index));
 		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.fragment_container, resultFragment)
+				.replace(R.id.fragment_container, resultFragment, "BROCA")
 				.commit();
 	}
 
 	@Override
 	public void onTryAgainButtonClicked(String tag) {
+		if (tag.equals("BROCA")) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, brocaIndexFragment)
+					.commit();
+		} else if (tag.equals("BMI")) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, bmiFragment)
+					.commit();
+		}
+
+
+	}
+
+	@Override
+	public void onCalculateBMIClicked(String result) {
+		resultFragment.setInformation(String.format("Your healthy BMI range is "+result));
 		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.fragment_container, brocaIndexFragment)
+				.replace(R.id.fragment_container, resultFragment, "BMI")
 				.commit();
 	}
 }
--- app/src/main/java/id/ac/polinema/idealbodyweight/fragment/BMIFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragment/BMIFragment.java
@@ -6,52 +6,59 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import id.ac.polinema.idealbodyweight.R;
+import id.ac.polinema.idealbodyweight.util.BodyMassIndex;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ResultFragment.OnFragmentInteractionListener} interface
+ * {@link BMIFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
-public class ResultFragment extends Fragment {
+public class BMIFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
-    private String information;
 
-    public ResultFragment() {
+    public BMIFragment() {
         // Required empty public constructor
     }
-    public void setInformation(String information) {
-        this.information = information;
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_result, container, false);
-        TextView informationText = view.findViewById(R.id.text_information);
-        informationText.setText(information);
-        Button tryAgainButton = view.findViewById(R.id.btn_tyr_again);
-        tryAgainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+        View view = inflater.inflate(R.layout.fragment_bmi,container, false);
+        final EditText massText = view.findViewById(R.id.input_mass);
+        final EditText heightText = view.findViewById(R.id.input_height);
+
+        Button calculateButton = view.findViewById(R.id.btn_calculateBMI);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 if (mListener != null) {
-                    Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment_container);
-                    mListener.onTryAgainButtonClicked(fragment.getTag());
+                    String massString = massText.getText().toString();
+                    String heightString = heightText.getText().toString();
+                    if (!TextUtils.isEmpty(massString) && !TextUtils.isEmpty(heightString)) {
+                        float mass = Float.parseFloat(massString);
+                        float height = Float.parseFloat(heightString);
+                        BodyMassIndex bmi = new BodyMassIndex(mass, height/100);
+                        mListener.onCalculateBMIClicked(bmi.BMIRange());
+                    } else {
+                        Toast.makeText(getActivity(), "Please Fill the form", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
-        return  view;
+        return view;
     }
+
+    // TODO: Rename method, update argument and hook method into UI event
 
     @Override
     public void onAttach(Context context) {
@@ -82,6 +89,6 @@ public class ResultFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onTryAgainButtonClicked(String tag);
+        void onCalculateBMIClicked(String result);
     }
 }
